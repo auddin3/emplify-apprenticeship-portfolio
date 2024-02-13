ce1be3fb5dee3fd307cef807a5eb66f07d940c22
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2024-02-13 15:31:22 +0000

    FSENG-02: Connect to Emplify DB

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let database
 
 async function connect() {
     const client = await MongoClient.connect('mongodb://127.0.0.1:27017')
-    database = client.db('blog')
+    database = client.db('emplify')
 }
 
 function getDb() {
