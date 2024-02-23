a69a23759bc5c35b9aa7e459ce8b81ba97ebd5b6
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2024-02-23 16:54:23 +0000

    Configure eslint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../service/user');
+const express = require("express")
+const router = express.Router()
+const userController = require("../service/user")
 
 // Define user-related routes
-router.get('/', userController.getAllUsers);
-router.get('/:userId', userController.getUserById);
-router.post('/', userController.createUser);
-router.put('/:userId', userController.updateUser);
-router.delete('/:userId', userController.deleteUser);
+router.get("/", userController.getAllUsers)
+router.get("/:userId", userController.getUserById)
+router.post("/", userController.createUser)
+router.put("/:userId", userController.updateUser)
+router.delete("/:userId", userController.deleteUser)
 
-module.exports = router;
+module.exports = router
