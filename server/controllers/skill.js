1b478a6dcf6c2329db9fb913c10acc191c08c4ef
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2024-02-25 18:07:40 +0000

    RM console.log

--- server/controllers/skill.js
+++ server/controllers/skill.js
@@ -5,7 +5,6 @@ const collectionName = 'skills'
 const getSkills = async (skillIds) => {
   const db = getDb()
 
-  console.log(skillIds)
   const skills = await db.collection(collectionName).find({ title: { $in: skillIds } }).toArray()
   return { skills }
 }
