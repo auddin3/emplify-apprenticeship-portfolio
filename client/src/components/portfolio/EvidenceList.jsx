ba8cf150c78664a4f660fb8da67f713c75b53116
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2024-04-20 19:49:42 +0100

    Icons for Filetype

--- client/src/components/portfolio/EvidenceList.jsx
+++ client/src/components/portfolio/EvidenceList.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
-import { Card, CardHeader, CardBody, IconButton, Stack, StackDivider } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, IconButton, Icon, Stack, StackDivider } from '@chakra-ui/react'
 import { ArrowPathIcon } from '@heroicons/react/24/outline'
+import { FolderOpenIcon, DocumentChartBarIcon, DocumentIcon, PresentationChartBarIcon } from '@heroicons/react/24/solid'
 
 const EvidenceList = ({ openFile, fileList, selectedKSB }) => {
   const renderFileList = (files) => {
     return (
       <Stack divider={<StackDivider />}>
         {files?.map((file, index) => (
-          <div key={index}>
+          <div key={index} className='px-3'>
             {file.isDirectory
               ? (
-                <div>
-                  {file.name}
-                  {renderFileList(file.files)}
+                <div className='flex flex-row space-x-3'>
+                  <Icon as={FolderOpenIcon} />
+                  <div>
+                    {file.name}
+                    {renderFileList(file.files)}
+                  </div>
                 </div>
               )
               : (
-                <div>{file.name}</div>
+                <div className='flex flex-row space-x-3 items-center'>
+                  {['pdf', 'xlsx'].includes(file.name.split('.')[1]) && <Icon as={DocumentChartBarIcon} color="#333D49"/>}
+                  {['docx', 'doc'].includes(file.name.split('.')[1]) && <Icon as={DocumentIcon} color="#333D49"/>}
+                  {['pptx', 'ppt'].includes(file.name.split('.')[1]) && <Icon as={PresentationChartBarIcon} color="#333D49"/>}
+                  <div>{file.name.split('.')[0]}</div>
+                </div>
               )}
           </div>
         ))}
