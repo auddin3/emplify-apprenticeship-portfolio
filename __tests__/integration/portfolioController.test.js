4dad87523783c82ad1f0eb16d44c6e0e4d6f69f6
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2024-04-27 13:23:41 +0100

    Portfolio integration tests

--- __tests__/integration/portfolioController.test.js
+++ __tests__/integration/portfolioController.test.js
@@ -4,7 +4,7 @@ const db = require('../../server/config/database')
 const API_CONNECTION = 'http://localhost:5001'
 const COLLECTION_NAME = 'portfolios'
 
-describe('Register Controller Integration Tests', () => {
+describe('Portfolio Controller Integration Tests', () => {
     beforeAll(async () => {
         await db.connectToDatabase()
     })
@@ -29,21 +29,28 @@ describe('Register Controller Integration Tests', () => {
         })
     })
 
-    describe('updatePortfolio', () => {
-        it('should update portfolio', async () => {
-            const response = await request(API_CONNECTION)
-            .post(`/portfolio/65db659ad8c16c7a6b9974fc`)
-            .send({ 
-                name: 'Updated Title', 
-                description: 'This is a test', 
-                specification: ['C1', 'C4'] 
-            })
+    // describe('updatePortfolio', () => {
+    //     it('should update portfolio', async () => {
+    //         const response = await request(API_CONNECTION)
+    //         .post(`/portfolio/65db659ad8c16c7a6b9974fc`)
+    //         .send({ 
+    //             name: 'Updated Title', 
+    //             description: 'This is a test', 
+    //             specification: ['C1', 'C4'] 
+    //         })
 
-            expect(response.status).toBe(200)
-            expect(response.body.portfolio.name).toBe('Updated Title')
-            expect(response.body.portfolio.description).toBe('This is a test')
-            expect(response.body.portfolio.specification).toEqual(['C1', 'C4'] )
-        })
+    //         expect(response.status).toBe(200)
+    //         expect(response.body.portfolio.name).toBe('Updated Title')
+    //         expect(response.body.portfolio.description).toBe('This is a test')
+    //         expect(response.body.portfolio.specification).toEqual(['C1', 'C4'] )
+    //     })
+    // })
+
+    describe('getEntries', () => {
+      it('should return all portfolio entries', async () => {
+        const response = await request(API_CONNECTION).get('/entries')
+        expect(response.status).toBe(200)
+      })
     })
 
     describe('getPortfolioEntries', () => {
@@ -52,33 +59,54 @@ describe('Register Controller Integration Tests', () => {
           const response = await request(API_CONNECTION).get(`/portfolio/65db659ad8c16c7a6b9974fc`)
           expect(response.status).toBe(200)
         })
-      })
+    })
 
-      describe('getEntries', () => {
-        it('should return all portfolio entries', async () => {
-          const response = await request(API_CONNECTION).get('/entries')
-          expect(response.status).toBe(200)
-        })
+    describe('insertPortfolioEntry', () => {
+      it('should insert a new portfolio entry', async () => {
+        
+        const response = await request(API_CONNECTION)
+          .put(`/portfolioEntry/65db659ad8c16c7a6b9974fc`)
+          .send({ 
+            skill: 'C1', 
+            module: 'MOCK101', 
+            dateCreated: new Date(), 
+            q1: 'test',
+            q2: 'test',
+            q3: 'test',
+            q4: 'test',
+            user: '65e346143c16c0c823b13fe5',
+            mockAssessment: 0.4,
+          })
+  
+        expect(response.status).toBe(200)
       })
+    })
 
-    //   describe('insertPortfolioEntry', () => {
-    //     it('should insert a new portfolio entry', async () => {
-    //       const response = await request(API_CONNECTION)
-    //         .put(`/portfolioEntry/65db659ad8c16c7a6b9974fc`)
-    //         .send({ 
-    //             skill: 'New Skill', 
-    //             module: 'New Module', 
-    //             dateCreated: new Date(), 
-    //             q1: 'test',
-    //             q2: 'test',
-    //             q3: 'test',
-    //             q4: 'test'
-    //         })
-    
-    //       expect(response.status).toBe(200)
-    //       expect(response.body.entries.length).toBe(1)
-    //     })
-    //   })
+    describe('updatePortfolioEntry', () => {
+      it('should update a portfolio entry', async () => {
+        const entry = await db.getDb().collection('portfolioEntries').findOne({ module: 'MOCK101' })
+        const response = await request(API_CONNECTION)
+          .post(`/portfolioEntry/${entry._id}`)
+          .send({ 
+            q1: 'new test',
+            q2: 'new test',
+            q3: 'new test',
+            q4: 'new test',
+          })
+
+        expect(response.status).toBe(200)
+        expect(response.body.find(e => e.module === entry.module).q1).toBe('new test')
+      })
+    })
+
+    describe('deletePortfolioEntry', () => {
+      it('should delete a portfolio entry', async () => {
+        const entry = await db.getDb().collection('portfolioEntries').findOne({ module: 'MOCK101' })
+
+        const response = await request(API_CONNECTION).delete(`/portfolioEntry/${entry._id}`)
+        expect(response.status).toBe(200)
+      })
+    })
 
   }
 )
